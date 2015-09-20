6a4fe6ad1ebd08711afe287d07a2caaa56839a4a
kevbet <kevbet@e8e90e9a-3a0c-eeba-f2b6-c8d23dc220ac>
2015-09-20 12:58:54 +0000

    add in web public

--- BizChat/web/public/js/roomlist.js
+++ BizChat/web/public/js/roomlist.js
@@ -1,17 +1,43 @@
 var roomlist='<ul class=\"tree\">' +
-        '<li><a href=\"#\" ref=\"jdhy\"><strong>房产</strong></a>(<span>20</span>)</li>' +
-        '<ul show=\"true\">' +
-        '<li class="hot"><a href=\"#\" ref=\"xixi\">租赁</a></li>' +
-        '<li><a href=\"#\" ref=\"hehe\">买卖</a></li>' +
-        '</ul>' +
-        '<li><a href=\"#\" ref=\"oo\"><strong>市区</strong>(<span>20</span>)</a></li>' +
-        '<ul>' +
-        '<li><a href=\"#\" ref=\"hh\"></a></li>' +
-        '<li><a href=\"#\" ref=\"cc\">普陀</a></li>' +
-        '<li><a href="#" ref="sb">静安</a></li>' +
-        '<ul>' +
-        '<li><a href=\"#\" ref=\"s\">汽车</a>(<span>20</span>)</li>' +
-        '<li><a href=\"#\" ref=\"esndk\">喀什能打开</a></li>' +
-        '</ul>' +
-        '</ul>' +
-        '</ul>';
+'<li><a href="#" ref="oo">交友</a></li>'+
+'<li><a href="#" ref="oo">医疗健康</a></li>'+
+'<li><a href="#" ref="oo">十三张</a></li>'+
+'<li><a href="#" ref="oo">大厅</a></li>'+
+'<li><a href="#" ref="oo">小商品和二手货</a></li>'+
+'<li><a href="#" ref="oo"><strong>房产</strong></a>(<span>20</span>)</li>'+
+'<ul show="true">'+
+'<li><a href="#" ref="oo"><strong>买卖</strong></a>(<span>20</span>)</li>'+
+'<ul show="true">'+
+'<li><a href="#" ref="oo">普陀</a></li>'+
+'<li><a href="#" ref="oo">静安</a></li>'+
+'<li><a href="#" ref="oo">黄埔</a></li>'+
+'</ul>'+
+'<li><a href="#" ref="oo"><strong>租赁</strong></a>(<span>20</span>)</li>'+
+'<ul show="true">'+
+'<li><a href="#" ref="oo"><strong>浦东</strong></a>(<span>20</span>)</li>'+
+'<ul show="true">'+
+'<li><a href="#" ref="oo">中心</a></li>'+
+'<li><a href="#" ref="oo">市郊</a></li>'+
+'</ul>'+
+'<li><a href="#" ref="oo"><strong>浦西</strong></a>(<span>20</span>)</li>'+
+'<ul show="true">'+
+'<li><a href="#" ref="oo">中心</a></li>'+
+'<li><a href="#" ref="oo">市郊</a></li>'+
+'</ul>'+
+'</ul>'+
+'</ul>'+
+'<li><a href="#" ref="oo"><strong>教育</strong></a>(<span>20</span>)</li>'+
+'<ul show="true">'+
+'<li><a href="#" ref="oo">数学</a></li>'+
+'<li><a href="#" ref="oo"><strong>编程</strong></a>(<span>20</span>)</li>'+
+'<ul show="true">'+
+'<li><a href="#" ref="oo">C++</a></li>'+
+'<li><a href="#" ref="oo">Java</a></li>'+
+'<li><a href="#" ref="oo">Ruby</a></li>'+
+'</ul>'+
+'<li><a href="#" ref="oo">英语</a></li>'+
+'</ul>'+
+'<li><a href="#" ref="oo">派对</a></li>'+
+'<li><a href="#" ref="oo">职业</a></li>'+
+'<li><a href="#" ref="oo">车市</a></li>'+
+'<li><a href="#" ref="oo">餐饮&酒吧</a></li>';
